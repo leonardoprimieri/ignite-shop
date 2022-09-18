8028bb77b1d3d32d1e18579c5b1219f378fcc28f
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-09-18 18:26:17 -0300

    feat: add Container and Header components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import { globalStyles } from "../styles/global";
 
-export default MyApp;
+globalStyles();
+
+import logoImg from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "../styles/pages/app-styles";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} />
+      </Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
+}
