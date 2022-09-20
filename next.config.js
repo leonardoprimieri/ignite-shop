8b8daa0aae732db987a4b891062c7c9b58ebb2f8
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-09-20 09:36:44 -0300

    feat: redirect user if has no session_id

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
 
   images: {
-    domains: ["files.stripe.com", "source.unsplash.com"]
+    domains: ["files.stripe.com"]
   }
 }
 
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,27 +1,62 @@
+import { GetServerSideProps } from "next"
 import Image from "next/future/image"
 import Link from "next/link"
-import { ProductImageContainer } from "../styles/pages/product-page-styles"
+import Stripe from "stripe"
+import { stripeApi } from "../lib/stripe"
 import {
   SuccessPageContainer,
   SuccessProductContainer
 } from "../styles/pages/success-page-styles"
 
-export default function SuccessPage() {
-  // random unsplash image url
-  const randomImage = "https://source.unsplash.com/random/800x600"
+type Props = {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
 
+export default function SuccessPage({ customerName, product }: Props) {
   return (
     <SuccessPageContainer>
       <h1>Compra efetuada!</h1>
       <SuccessProductContainer>
-        <Image src={randomImage} alt='' width={200} height={200} />
+        <Image src={product.imageUrl} alt='' width={200} height={200} />
       </SuccessProductContainer>
       <p>
-        Uhuul <strong>Leonardo M. Primieri</strong>, sua{" "}
-        <strong>Camiseta</strong> já está a caminho da sua casa.
+        Uhuul <strong>{customerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href='/'>Voltar ao catálogo</Link>
     </SuccessPageContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false
+      }
+    }
+  }
+
+  const sessionId = String(query.session_id)
+  const session = await stripeApi.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"]
+  })
+
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  return {
+    props: {
+      customerName: session.customer_details.name,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0]
+      }
+    }
+  }
 }
